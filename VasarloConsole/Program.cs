c7547558221bb0f809f6d3a2dbaa2e4d34191e74
2020b Gavlik Martin <144203996+b0gavmar@users.noreply.github.com>
2025-05-14 15:08:12 +0200

    b feladatrész 1-2. kész

--- VasarloConsole/Program.cs
+++ VasarloConsole/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.Linq.Expressions;
 using VasarloConsole.Models;
+using VasarloConsole.Repos;
 
 Console.WriteLine("\n 1. feladat");
 try
@@ -56,19 +57,38 @@ catch (Exception ex)
 }
 
 Console.WriteLine("\n 4. feladat");
+
+Vasarlo v2 = new Vasarlo("vasarlo2mail", 0);
+Vasarlo v3 = new Vasarlo("vasarlo3mail", 300);
+Vasarlo v4 = new Vasarlo("vasarlo4mail", 400);
+Vasarlo v5 = new Vasarlo("vasarlo5mail", 500);
+Console.WriteLine(v2);
+Console.WriteLine(v3);
+Console.WriteLine(v4);
+Console.WriteLine(v5);
+
+
+Console.WriteLine("\n B) feladatérsz 1-2. feladat");
+VasarloRepo repo = new VasarloRepo();
+repo.AddVasarlo(valer);
+repo.AddVasarlo(v2);
+repo.AddVasarlo(v3);
+repo.AddVasarlo(v4);
+repo.AddVasarlo(v5);
+
+Console.WriteLine($"A tárolt vásárlók száma: {repo.Vasarlok.Count} db");
+
+foreach (Vasarlo v in repo.Vasarlok)
+{
+    Console.WriteLine(v);
+}
+
 try
 {
-    Vasarlo v2 = new Vasarlo("vasarlo2mail", 0);
-    Vasarlo v3 = new Vasarlo("vasarlo3mail", 300);
-    Vasarlo v4 = new Vasarlo("vasarlo4mail", 400);
-    Vasarlo v5 = new Vasarlo("vasarlo5mail", 500);
-    Console.WriteLine(v2);
-    Console.WriteLine(v3);
-    Console.WriteLine(v4);
-    Console.WriteLine(v5);
+    repo.RemoveVasarlo("vasarlo3mail");
 }
-catch(Exception ex)
+catch (Exception ex)
 {
     Console.WriteLine(ex.Message);
 }
-
+Console.WriteLine($"A tárolt vásárlók száma 1 törlés után: {repo.Vasarlok.Count} db");
---
+++ VasarloConsole/Repos/VasarloRepo.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VasarloConsole.Models;
+
+namespace VasarloConsole.Repos
+{
+    public class VasarloRepo
+    {
+        private List<Vasarlo> _vasarlok = new List<Vasarlo>();
+
+        public List<Vasarlo> Vasarlok { get => _vasarlok; }
+
+        public void AddVasarlo(Vasarlo vasarlo)
+        {
+            foreach(Vasarlo v in _vasarlok)
+            {
+                if (v.Email == vasarlo.Email)
+                {
+                    throw new Exception("Ez az email már létezik");
+                }
+            }
+            _vasarlok.Add(vasarlo);
+        }
+
+        public void RemoveVasarlo(string email)
+        {
+            List<string> emails = new List<string>();
+            foreach (Vasarlo v in _vasarlok)
+            {
+                emails.Add(v.Email);
+            }
+
+            if(emails.Contains(email))
+            {
+                Vasarlo vasarlo = _vasarlok.FirstOrDefault(v => v.Email == email);
+                if (vasarlo != null)
+                {
+                    _vasarlok.Remove(vasarlo);
+                }
+            }
+            else
+            {
+                throw new Exception("Ez az email nem létezik");
+            }
+        }
+    }
+}
